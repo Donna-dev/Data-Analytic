25dc185cd709fc06c06999c16e23751b989bf69d
Donna-dev <donnamae.bagotlo@bisu.edu.ph>
2024-12-21 23:41:08 +0800

    Add files via upload

---
+++ app/Http/Controllers/admin/auth/loginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class loginController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('resources/views/profile/login.blade.php'); // Create resources/views/auth/login.blade.php
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->route('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'Invalid credentials.',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        return redirect()->route('Login');
+    }
+}
